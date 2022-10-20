7ffbfe4e9aa497980cd55816667fef14c99fd771
SamNHipp <115831553+SamNHipp@users.noreply.github.com>
2022-10-19 19:58:38 -0700

    Update Fraction.java

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -38,14 +38,21 @@ public class Fraction {
    }
 
    public static void main (String args[]) {
+      
+      int num = 0;
+      int denom = 0;
+      
+      
       try {
          // create a new instance
          // Fraction *frac = [[Fraction alloc] init];
          Fraction frac = new Fraction();
+         num =Integer.parseInt(args[0]);
+         denom = Integer.parseInt(args[1]);
 
          // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
+         frac.setNumerator(num);
+         frac.setDenominator(denom);
 
          // print it
          System.out.print("The fraction is: ");
